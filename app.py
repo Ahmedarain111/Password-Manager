b3a17d1093659cca007aa6a842590be0b2d207ae
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-07-18 15:39:36 +0500

    Add imports

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 
